a39b0a7c617858aaf6294a7540d6786e52fe34c5
Miukki <miukki@gmail.com>
2023-02-19 22:27:48 +1100

    update frontend

--- dapp.config.js
+++ dapp.config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'Vipsland Dapp',
   description: 'Buy PRTs and Mint NFT',
-  contractAddress: '0xd0f64f031632d3C23Bcb416f76e03b4A0Ebf4E6d',
+  contractAddress: '0x131435E6fB258Ab1ED27250adC77a58fF846B56D',
   maxMintAmount: 1,
   // pricePRT: 0.123,
   // priceNFT: 0
--- scripts/verifyContract.js
+++ scripts/verifyContract.js
@@ -16,7 +16,7 @@ const hre = require('hardhat')
 async function main() {
 
   await hre.run('verify:verify', {
-    address: '0xd0f64f031632d3C23Bcb416f76e03b4A0Ebf4E6d',//latest
+    address: '0x131435E6fB258Ab1ED27250adC77a58fF846B56D',//latest
     constructorArguments: []
   })
 }
