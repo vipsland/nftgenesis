e3dade1f7956f4834ac8d286c64e027989e83aaa
Anja Ishmukhametova <miukki@gmail.com>
2023-04-02 22:23:14 +1000

    add fix

---
+++ pages/Footer.js
@@ -0,0 +1,27 @@
+
+
+export default function Footer() {
+
+  return (
+
+
+
+    <footer class="bg-white  m-4">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+
+          <span>
+            Unlock exclusive benefits by purchasing NFTs in sequence! <br />
+            Our smart contract algorithm rewards bulk purchases with increasing discounts and a higher chance of receiving a Membership Pass.<br />
+            Generally, buying 18 NFTs in sequence qualifies you for a Membership Pass, although it's not guaranteed, your chances are greatly increased. <br />
+            Don't miss out on the opportunity to gain exclusive access and save on your NFT purchases!
+          </span>
+
+        </span>
+      </div>
+    </footer>
+
+
+
+  )
+}
